45624f29a96bb28a82193ea6e90cbdfd5d411e6b
Pedro Dantas <pedrojr_dantas@hotmail.com>
2021-08-27 07:50:47 +0200

    asyncWithNextTick

--- index.js
+++ index.js
@@ -14,12 +14,20 @@ console.log('end') ; */
 //sencond exemple
 
 function hideString(str, done) {
-    done(str.replace(/[a-zA-Z]/g, 'X'));
+    process.nextTick(() => {
+        done(str.replace(/[a-zA-Z]/g, 'X'));
+    })
+    
 }
 hideString("Hello World", (hidden) => {
     console.log( hidden );
 });
 
 console.log( 'end' );
+
+
+// to be a callback it needs to be async
+// process.nextTick () logs the hideString in the next loop, in the next tick
+// the done function inside the process.nextTick() is the callback
 
 
