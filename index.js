ccf5e496d82394fd526b1dbd15b39605ec0a577a
Pedro Dantas <pedrojr_dantas@hotmail.com>
2021-08-27 07:36:26 +0200

    synchronously

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-function hideString (str) {
-    return str.reaplace(/[a-zA-Z]/g, 'X');
+
+//first exemple
+
+/* function hideString (str) {
+    return str.replace(/[a-zA-Z]/g, 'X');
 }
 
 var hidden = hideString("Hello World");
 
 console.log( hidden );
+
+console.log('end') ; */
+
+//sencond exemple
+
+function hideString(str, done) {
+    done(str.replace(/[a-zA-Z]/g, 'X'));
+}
+hideString("Hello World", (hidden) => {
+    console.log( hidden );
+});
+
+console.log( 'end' );
+
 
